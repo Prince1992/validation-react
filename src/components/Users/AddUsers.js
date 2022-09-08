bfc19f2f62b77070a97cec674511518cc54fd60b
Amar Singh Yadav <amarprince9@gmail.comm>
2022-09-08 11:06:24 +0530

    Styled Components

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Button } from '../UI/Button';
+import { Card } from '../UI/Card';
+import classes from './AddUsers.module.css';
 
 const AddUsers = () => {
   const addUserhandler = (e) => {
     e.preventDefault();
   };
   return (
-    <form onSubmit={addUserhandler}>
-      <label htmlFor="username">Username</label>
-      <input type="text" id="username" />
-      <label htmlFor="age">Age (Years)</label>
-      <input type="number" id="age" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserhandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+        <label htmlFor="age">Age (Years)</label>
+        <input type="number" id="age" />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
   );
 };
 
