a294cecec282a96a33185b27aa3fdf2c1e4d3cca
Amar Singh Yadav <amarprince9@gmail.comm>
2022-09-10 11:14:14 +0530

    React Fragment

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
     });
   };
   return (
-    <div>
+    <>
       <AddUsers onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </>
   );
 }
 
